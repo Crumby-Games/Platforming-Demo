ebd1703ef674aea47cb418df3f3407f327a8833d
Crumby Games <79009277+Crumby-Games@users.noreply.github.com>
2026-06-23 13:29:27 +0100

    Reorganised
    Renamed several files and moved them into cleaner subfolders

--- Misc/level.gd
+++ Misc/level.gd
@@ -10,10 +10,10 @@ var optionals_collected = 0
 
 
 func collect_optional() -> void:
-    optionals_collected += 1
+	optionals_collected += 1
 
 func collect_flag() -> void:
-    flags_collected += 1
-    if flags_collected >= flags_required:
-        level_complete.emit(optionals_collected)
-        World.load_menu()
+	flags_collected += 1
+	if flags_collected >= flags_required:
+		level_complete.emit(optionals_collected)
+		World.load_menu()
